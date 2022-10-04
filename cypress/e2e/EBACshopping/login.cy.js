96c643cd9630bb4bb23fb4268ac13dc8f2869b01
Vinicius Sousa <85960539+viniciussousa16@users.noreply.github.com>
2022-10-03 21:04:38 -0300

    Aula automação de teste usando faker e hook

--- cypress/e2e/EBACshopping/login.cy.js
+++ cypress/e2e/EBACshopping/login.cy.js
@@ -2,8 +2,15 @@
 
 context ('Funcionalidade Login', () =>{
 
-    it ('Deve realizar login com sucesso', () => {
+    beforeEach(() => {
         cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it ('Deve realizar login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -14,7 +21,6 @@ context ('Funcionalidade Login', () =>{
     })
 
     it ('Deve exibir menssagem de erro ao inserir senha inválida', () => {
-        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('ehsheyy')
         cy.get('.woocommerce-form > .button').click()
@@ -23,7 +29,6 @@ context ('Funcionalidade Login', () =>{
     })
 
     it ('Deve exibir menssagem de erro ao inserir dados inválidos', () => {
-        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_123456ssebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
