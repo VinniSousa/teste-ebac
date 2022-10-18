095df4aae27926b12e591516a7804fceba01caf2
Vinicius Sousa <85960539+viniciussousa16@users.noreply.github.com>
2022-10-17 21:23:37 -0300

    Alterações de teste automatizado

---
+++ cypress/e2e/EBACshopping/produto.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first() Pega o primeiro item da classe
+            //.last() Pega o ultimo item da classe
+            //.eq(3) Pega o item X da classe
+            .contains('Arcadio Gym Short') //Pega pelo texto do botão da classe
+            .click()
+    });
+
+    it('deve adicionar um produto ao carrinho', () => {
+        var quant=5
+
+        cy.get('[class="product-block grid"]')
+        .contains('Ajax Full-Zip Sweatshirt').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(quant)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quant)
+        cy.get('.woocommerce-message').should('contain', quant + ' × “Ajax Full-Zip Sweatshirt”')
+    });
+});
