992cdc2885aa7ad8a5e111c5a2140459bd2f900a
Vinicius Sousa <85960539+viniciussousa16@users.noreply.github.com>
2023-02-22 16:57:26 -0300

    Automação de teste com massa de dados em listas (Arrays)

--- cypress/e2e/EBACshopping/endereco.cy.js
+++ cypress/e2e/EBACshopping/endereco.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 import enderecoPages from '../../support/pages-objects/endereco.pages';
 import EnderecoPages from '../../support/pages-objects/endereco.pages'
+const dadosEnderecos = require ('../../fixtures/endereco.json')
 
 describe('Funcionalidade Enredeços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -11,7 +12,25 @@ describe('Funcionalidade Enredeços - Faturamento e Entrega', () => {
     });
 
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
        enderecoPages.editarEnderecoFaturamento('nome','sobrenome','ebac','Brasil','Av. Teste','123','Curitiba','Paraná','82590-300','080000000','teste@teste.com.br')
+       cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
     });
+    
+    it('Deve fazer cadastro de faturamento com sucesso usando arquivo de dados', () => {
+      enderecoPages.editarEnderecoFaturamento(
+      dadosEnderecos[2].nome,
+      dadosEnderecos[2].sobrenome,
+      dadosEnderecos[2].empresa,
+      dadosEnderecos[2].pais,
+      dadosEnderecos[2].endereco,
+      dadosEnderecos[2].numero,
+      dadosEnderecos[2].cidade,
+      dadosEnderecos[2].estado,
+      dadosEnderecos[2].cep,
+      dadosEnderecos[2].telefone,
+      dadosEnderecos[2].email
+      )
+      cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
+   });
 });
